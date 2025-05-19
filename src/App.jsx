107cc22b3ae50e9527de7678486e4113d8dbbc50
FraGal05 <46552826@roquegonzalez.org>
2025-05-18 21:54:06 -0300

    FRANCISCO GALARZA

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import PokeCard from './assets/components/FRANCISCO-GALARZA/PokeCard'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [poke, setPoke] = useState ("")
+  const [mostrar, setMostrar] = useState (false)
   return (
     <>
+    <input type="text" 
+    placeholder="Escriba un nombre" 
+    value={poke} 
+    onChange={(event)=>setPoke(event.target.value)} />
+    <button onClick={()=>setMostrar(!mostrar)}>{mostrar ? "Ocultar Carta":"Mostrar Carta"}</button>
+    {mostrar && poke &&
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <PokeCard pokeUrl={`https://pokeapi.co/api/v2/pokemon/${poke}`}/>
+      </div>}
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/assets/components/FRANCISCO-GALARZA/PokeCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./PokeCard.css"
+
+function PokeCard ({pokeUrl}) {
+      const [pokemon, setPokemon] = useState();
+
+  useEffect (() => {      
+      fetch(pokeUrl)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+    }, [pokeUrl]);
+    return (
+      <>
+      <div className="pokeCardFondo">
+        <div className="pokeCard">
+          <div className = "pokeCardSuperior">
+            <label>{pokemon?.name.toUpperCase()} LV.10</label>
+            <label>HP <nav>{pokemon?.stats?.find((stat) => stat.stat.name === "hp")?.base_stat}</nav></label>
+          </div>
+          <img className="pokeCardImg" src={pokemon?.sprites.front_default} alt={pokemon?.name}/>
+          <div className="pokeCardInferior">
+            <div>
+            ABILITY: {pokemon?.abilities?.[0].ability.name.toUpperCase()}
+            </div>
+            <div className="pokeMovs">
+              MOVES: 
+              <ul>{pokemon?.moves?.[0]?.move.name.toUpperCase()}</ul>
+              <ul>{pokemon?.moves?.[1]?.move.name.toUpperCase()}</ul>
+              <ul>{pokemon?.moves?.[2]?.move.name.toUpperCase()}</ul>
+            </div>
+            <div className="pokeXp">
+              EXP: {pokemon?.base_experience}
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
+
+
+export default PokeCard
