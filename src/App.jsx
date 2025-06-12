2a4d81e953006b46f4fb23d23eebddc1501fd2de
evolution666x2 <natanros40@gmail.com>
2025-06-12 15:03:54 -0300

    style(app.css): added style for the navigation menu feat(app.jsx): added new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import BoosterSelector from "./components/BoosterSelector/BoosterSelector";
+import "./App.css"
 
 export default function App() {
   return (
-    <div className="App">
-      <BoosterSelector />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <NavLink to="/" end>
+          Inicio
+        </NavLink>{" "}
+        |<NavLink to="/abrir-sobre">Abrir Sobre</NavLink> |
+        <NavLink to="/mi-equipo">Mi Equipo</NavLink> |
+        <NavLink to="/favoritos">Favoritos</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" />
+        <Route path="/abrir-sobre" element={<BoosterSelector />} />
+        <Route path="/mi-equipo" />
+        <Route path="/favoritos" />
+      </Routes>
+    </BrowserRouter>
   );
 }
