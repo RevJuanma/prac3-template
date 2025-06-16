6d6093d47435708c5b29c3d0588caa03830b0cc2
FraGal05 <46552826@roquegonzalez.org>
2025-06-16 19:30:56 -0300

    Juego Sobres Pkmn

--- src/pages/Sobres/Sobres.jsx
+++ src/pages/Sobres/Sobres.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import './sobres.css'
+import { useOutletContext } from 'react-router'
+import { obtenerPkmnData } from '../../functions/PokeAPI'
 
 const BoosterScreen = () => {
+  const { puntos, accionCarta } = useOutletContext();
+
+  const abrirSobre = async (tipo) => {
+    const costo = tipo === 'basico' ? 5 : 8;
+    const cantidad = tipo === 'basico' ? 5 : 6;
+
+      if (puntos < costo) {
+        console.log("Puntos insuficientes");
+      return;
+    }
+
+    const ids = [];
+    while (ids.length < cantidad) {
+      Math.floor(Math.random() * 2000) + 1;
+    }
+
+    const cartas = await Promise.all(ids.map(id => obtenerPkmnData(id)));
+
+    const cartasValidas = cartas.filter(c => c !== null);
+
+
+    accionCarta('gastarPuntos', costo);
+    accionCarta('añadirNuevaCarta', cartasValidas);
+  
+  };
+  
+
+
   return (
     <>
     <div className='body'>
       <h1 className='title'>Abrir Sobres</h1>
       <div className='forms'>
-        <form className='sobre1'>
+        <form className='sobre1' onSubmit={e => { e.preventDefault(); abrirSobre('basico'); }}>
           <h3>Abrir Sobre Basico</h3>
           <nav>Costo: 5 puntos</nav>
           <ul>
@@ -16,7 +46,7 @@ const BoosterScreen = () => {
           </ul>
           <button>Abrir</button>
         </form>
-        <form className='sobre2'>
+        <form className='sobre2' onSubmit={e => { e.preventDefault(); abrirSobre('basico'); }}>
           <h3>Abrir Sobre Premium</h3>
           <nav>Costo: 8 puntos</nav>
           <ul>
