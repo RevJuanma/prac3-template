9bdef6ffa1047a66fa5c2267377dcf8782cce58e
Mereles Lautaro <lautaro.jobs@hotmail.com>
2025-06-29 02:42:08 -0300

    create register page, configure util/constant with the url of the API

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerUser } from '../store/auth/authThunks';
+
+import TextField from '../components/TextField';
+import ErrorMessage from '../components/ErrorMessage';
+import CenteredContainer from '../components/CenteredContainer';
+import AuthFormWrapper from '../components/AuthFormWrapper';
+
+const Register = () => {
+  const dispatch = useDispatch(); // hook para actualizar el estado global (Redux) (ej:acción que hace el registro)
+  const { error, loading } = useSelector(state => state.auth); // hook q permite seleccionar una parte del estado global  de redux
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(registerUser(formData)).unwrap(); // .unwrap() permite que la promesa lance una excepción si falla
+      console.log('Registro exitoso');
+      // redireccion aca despues al home
+    } catch (err) {
+      console.error('Error en el registro:', err);
+    }
+  };
+
+  return (
+    <CenteredContainer>
+      <AuthFormWrapper
+        title="Registro"
+        onSubmit={handleSubmit}
+        buttonLabel={loading ? 'Registrando...' : 'Registrarse'}
+        disabled={loading}
+      >
+        <TextField
+          label="Nombre"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Tu nombre de Maestro Pokémon"
+        />
+
+        <TextField
+          label="Email"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="email@ejemplo.com"
+        />
+
+        <TextField
+          label="Contraseña"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="********"
+        />
+
+        {error && <ErrorMessage message={error} />}
+      </AuthFormWrapper>
+    </CenteredContainer>
+  );
+};
+
+export default Register;
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_BASE_URL = 'http://localhost:8082/api/v1';
+
+export const REGISTER_URL = `${API_BASE_URL}/authentication/register`;
+export const LOGIN_URL = `${API_BASE_URL}/authentication/login`;
