a93e2f416d7920c7337ef9f93ac304e9fb042a21
Mereles Lautaro <lautaro.jobs@hotmail.com>
2025-06-30 15:20:31 -0300

    add margin top in component Simple Pagination

--- frontend/src/components/SimplePagination.jsx
+++ frontend/src/components/SimplePagination.jsx
@@ -1,7 +1,7 @@
 const SimplePagination = ({ page, totalPages, onPrev, onNext }) => (
   <div style={{ display: 'flex', gap: '1rem', justifyContent: 'center', margin: '1rem 0' }}>
     <button onClick={onPrev} disabled={page === 0}>« Anterior</button>
-    <span>Página {page + 1} de {totalPages}</span>
+    <span style={{ marginTop: '10px'}}>Página {page + 1} de {totalPages}</span>
     <button onClick={onNext} disabled={page + 1 >= totalPages}>Siguiente »</button>
   </div>
 );
